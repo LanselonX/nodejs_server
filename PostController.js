7e838ac73bad6200609dc15edf8fd3c472af97ed
Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>
2022-12-22 15:05:42 +0100

    Small updates

--- PostController.js
+++ PostController.js
@@ -1,63 +1,74 @@
 import Post from "./Post.js";
 import PostService from "./PostService.js";
+import mongoose from "mongoose";
 
 class PostController {
-    async create(req, res) {
-        try {
-            const post = await PostService.create(req.body)
-            res.json(post)
-        } catch (e) {
-            res.status (500).json(e)
-        }
+  async create(req, res) {
+    try {
+      const post = await PostService.create(req.body);
+      res.json(post);
+    } catch (e) {
+      res.status(500).json(e);
     }
+  }
 
-    async getAll(req, res) {
-       try {
-        const posts = await Post.find();
-        return res.json(posts);
-       } catch (e) {
-        res.status(500).json(e)
-       }
+  async getAll(req, res) {
+    try {
+      const posts = await Post.find();
+      return res.json(posts);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async getOne(req, res) {
-        try {
-            let id = req.params.id
-            console.log(req.params)
-            if (!id) {
-                throw new Error('не указан ID')
-            }
-            const post = await PostService.getOne(id);
-            console.log(post)
-            return res.json(post)
-        } catch (e) {
-         res.status(500).json(e)
-        }
+  }
+  async getOne(req, res) {
+    try {
+      let id = req.params.id;
+      const cast = mongoose.Types.ObjectId(id);
+      if (!id) {
+        throw new Error("wrong ID");
+      }
+      const post = await PostService.getOne(id);
+      return res.json(post);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ error: e.message });
+
+      //   const data = axios.get('/api/posts/8u2r8sjsf')
+
+      //   if(data.error) {
+      //     // draw empty screen
+      //   }
+
+      //   // draw user that we get
+      //   <h1>{data.author}</h1>
     }
-    async update(req, res) {
-        try {
-            const post = req.body
-            if (!post._id) {
-                res.status(400).json({message: 'Id не указан'})
-            }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
-            return res.json(updatedPost);
-        } catch (e) {
-         res.status(500).json(e)
-        }
+  }
+  async update(req, res) {
+    try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.json(updatedPost);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    async delete(req, res) {
-        try {
-            const {id} = req.params
-            if (!id) {
-                res.status(400).json({message: 'Id не указан'})
-            }
-            const post = await Post.findByIdAndDelete(id);
-            return res.json(post)
-        } catch (e) {
-         res.status(500).json(e)
-        }
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
+    } catch (e) {
+      res.status(500).json(e);
     }
+  }
 }
-
 
 export default new PostController();
--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import router from './router.js';
+import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
 
-const PORT = 5000;
-const DB_URL = 'mongodb+srv://user:user@cluster0.wl2dkah.mongodb.net/?retryWrites=true&w=majority'
+const PORT = 7004;
+const DB_URL =
+  "mongodb+srv://user:user@cluster0.wl2dkah.mongodb.net/?retryWrites=true&w=majority";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use('/api', router)
+app.use(express.json());
+app.use("/api", router);
 
-app.use(express.json())
+app.use(express.json());
 
-async function startApp() { 
-    try {
-        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
-        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
-    } catch (e) {
-        console.log(e)
-    }
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-startApp()
-
+startApp();
 
 // app.get('/', (req, res) => {
 //     res.status(200).json("Server Workde")
